1ea6dcbf71d34af9734b9a6a9132cd35f5f0b008
AKSHAY <akshayjadhav461999@gmail.com>
2020-08-10 14:39:12 +0530

    layout outline created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css";
 import Sidebar from "./components/Sidebar/Sidebar";
+import Feed from "./components/Feed/Feed";
+import Widgets from "./components/Widgets/Widgets";
 
 function App() {
   return (
     <div className="app">
       <Sidebar />
+      <Feed />
+      <Widgets />
     </div>
   );
 }
