974a73d011640fc486ccb7e6925d7776c23f3c32
AKSHAY <akshayjadhav461999@gmail.com>
2020-08-10 17:50:54 +0530

    widgets done

--- src/components/Widgets/Widgets.js
+++ src/components/Widgets/Widgets.js
@@ -1,9 +1,32 @@
 import React from "react";
 import "./Widgets.css";
+import {
+  TwitterTweetEmbed,
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+} from "react-twitter-embed";
+import SearchIcon from "@material-ui/icons/Search";
+
 function Widgets() {
   return (
     <div className="widgets">
-      <h2>Widgets</h2>
+      <div className="widgets__input">
+        <SearchIcon className="widgets__searchIcon" />
+        <input type="text" placeholder="Search Twitter" />
+      </div>
+      <div className="widgets__widgetContainer">
+        <h2>What's happening</h2>
+        <TwitterTweetEmbed tweetId={"1292180215360368648"} />
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="AKSHAY_257"
+          options={{ height: 400 }}
+        />
+        <TwitterShareButton
+          url={"https://twitter.com/AKSHAY_257"}
+          options={{ text: "#reactjs is awesome", via: "akshay" }}
+        />
+      </div>
     </div>
   );
 }
